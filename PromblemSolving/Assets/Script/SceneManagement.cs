4c29a31f67a395aee92261b35bcc7175e9d1e932
Dany Zaky <danyzakydany123@gmail.com>
2021-10-10 21:47:46 +0700

    Problem10

---
+++ PromblemSolving/Assets/Script/SceneManagement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public void buttonProblem1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void buttonProblem2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void buttonProblem3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void buttonProblem4()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void buttonProblem5()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void buttonProblem6()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void buttonProblem7()
+    {
+        SceneManager.LoadScene(7);
+    }
+
+    public void buttonProblem8()
+    {
+        SceneManager.LoadScene(8);
+    }
+
+    public void buttonProblem9()
+    {
+        SceneManager.LoadScene(9);
+    }
+}
