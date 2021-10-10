525638c80061e97014d3f6bd4778ec8723aeb73b
Dany Zaky <danyzakydany123@gmail.com>
2021-10-10 16:45:08 +0700

    problem 6

---
+++ PromblemSolving/Assets/Script/SpawnerEnemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerEnemy : MonoBehaviour
+{
+    public int numberToSpawn;
+    public List<GameObject> spawnPool;
+    public GameObject spawnArea;
+
+    void Start()
+    {
+        spawnEnemy();
+    }
+
+    void spawnEnemy()
+    {
+        int randomItem = 0;
+        GameObject toSpawn;
+        PolygonCollider2D c = spawnArea.GetComponent<PolygonCollider2D>();
+
+        float screenX, screenY;
+        Vector2 pos;
+
+        for (int i = 0; i < numberToSpawn; i++)
+        {
+            randomItem = Random.Range(0, spawnPool.Count);
+            toSpawn = spawnPool[randomItem];
+
+            screenX = Random.Range(c.bounds.min.x, c.bounds.max.x);
+            screenY = Random.Range(c.bounds.min.y, c.bounds.max.y);
+
+            pos = new Vector2(screenX, screenY);
+            Instantiate(toSpawn, pos, toSpawn.transform.rotation);
+        }
+    }
+
+    void destroyEnemy()
+    {
+        foreach(GameObject o in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            Destroy(o);
+        }
+    }
+}
