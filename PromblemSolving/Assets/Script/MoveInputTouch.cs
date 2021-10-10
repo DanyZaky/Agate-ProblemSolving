4e7a9c80cc6821371e1733fc5a8fec50a73ca38d
Dany Zaky <danyzakydany123@gmail.com>
2021-10-10 12:33:50 +0700

    problem5

---
+++ PromblemSolving/Assets/Script/MoveInputTouch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveInputTouch : MonoBehaviour
+{
+    private Rigidbody2D Rb;
+    public float speed = 10;
+    Vector2 lastClickedPos;
+    [SerializeField]
+    bool moving;
+
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moving = true;
+        }
+        if (moving && (Vector2)transform.position != lastClickedPos)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position =  Vector2.MoveTowards(transform.position, lastClickedPos, step);
+        }
+        else
+        {
+            moving = false;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            moving = false;
+        }
+    }
+}
